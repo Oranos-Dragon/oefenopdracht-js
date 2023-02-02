f53ca60b81afa4856383604519390f2cbfa3915a
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2023-02-02 11:56:54 +0100

    opdracht 3

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ let post = {//opdracht 2
     date: "",
     views: 10
 }
+const pizzaOne = {//opdracht 3
+    pizza: "Cheeze Pizza",
+    description: "The cheeze pizza is from italy and is good for if you dont want alot on your pizza.",
+    ingrediants: "Cheeze, Tomatoe sauce",
+}
+const PizzaTwo = {
+    pizza: "Salami Pizza",
+    description: "The salami pizza first originated from italy and now is popoular around the entire world.",
+    ingrediants: "Salami, Cheese, Tomatoe sauce"
+}
+const PizzaThree = {
+    pizza: "4 Cheezes",
+    description: "This pizza has 4 different cheezes, what more do you want.",
+    ingrediants: "Cheeze, Cheeze, Cheeze, Cheeze, Tomatoe sauce"
+}
+const pizzas = [pizzaOne, PizzaTwo, PizzaThree]
