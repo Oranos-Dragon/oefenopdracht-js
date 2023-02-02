2cb9a170f5ae5098ce5e27d1e618cb392dd3f466
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2023-02-02 11:47:52 +0100

    opdracht 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 //objects
-let player = {
+let person = {//opdracht 1
     firstname: "",
     surname: "",
     age: "",
     school: "",
     score: ""
+}
+let post = {//opdracht 2
+    username: "",
+    title: "",
+    story: "",
+    date: "",
+    views: 10
 }
