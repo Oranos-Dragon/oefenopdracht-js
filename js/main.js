6ad919ce0ef26e7fa839957caf3a23571878cd40
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2023-02-02 12:12:49 +0100

    opdracht 4

--- js/main.js
+++ js/main.js
@@ -28,4 +28,40 @@ const PizzaThree = {
     description: "This pizza has 4 different cheezes, what more do you want.",
     ingrediants: "Cheeze, Cheeze, Cheeze, Cheeze, Tomatoe sauce"
 }
-const pizzas = [pizzaOne, PizzaTwo, PizzaThree]
+const pizzas = [pizzaOne, PizzaTwo, PizzaThree];
+//auto's
+let opel1 = {
+    title: "Opel Corsa 1.4 Turbo OPC-Line CARPLAY NAV PANO DAB XENON STU",
+    description: "IntelliLink Pakket, OPC Line Pack, Winter-pakket 2, Cosmo+ pakket, Achteruitrijcamera, Android Auto, Apple Carplay, bi-xenon koplampen, Dab, Dodehoek detector, Elektrisch schuif-/kanteldak.",
+    price: 13944.00,
+    kilometer: 45320,
+    buildDate: 2016
+}
+let Mercedes = {
+    title: "Mercedes-Benz B-klasse 200 Prestige AMG CARBON/LM18/LEER/NAV",
+    description: "Beste klant, voor meer informatie over deze auto vragen wij u telefonisch contact te nemen met ons, e-mails worden niet dagelijks gelezen. Dus graag even bellen naar 06 12 777 808 of 010 795 34 61",
+    price: 29950.00,
+    kilometer: 44293,
+    buildDate: 2016
+}
+let BMW = {
+    title: "BMW Z4 Roadster sDrive20i High Executive M Sport Automaat",
+    description: "M sport, Parking Pack, Safety-pack, BMW Z4 Roadster sDrive20i High Executive M Sport Automaat / BMW M 50 Jahre uitvoering / Adaptieve LED / Harman Kardon / Live Cockpit Professional / M sportstoelen / Comfort Access",
+    price: 71375.00,
+    kilometer: 1,
+    buildDate: 2023
+}
+let Opel2 = {
+    title: "Opel Corsa 1.2 16V 5D 2014 Bruin / 2tone",
+    description: "Super zuinige en mooie auto in een bijzondere 2 tone kleur. Binnenkant in top staat.Full options! Weinig kilometers. Ennn.....altijd van een oude man geweest.",
+    price: 7950.00,
+    kilometer: 80000,
+    buildDate: 2023
+}
+let opel3 = {
+    title: "De mooiste Opel Mokka-e 's | GS Line | Elegance | 3-Fase",
+    description: "Bezoek onze website (via button/link) waar u onze uitgebreide voorraad aan Opel Mokka's vindt. Hier kunt u de auto's bekijken op ruim 60 foto's en via onze 360 graden film van binnen en buiten. Ook actuele verkoopprijzen en alle specificaties staan hier overzichtelijk gepresenteerd. Al deze auto's zijn per direct beschikbaar en uiteraard ook online te bestellen.",
+    price: 34895.00,
+    kilometer: 0,
+    buildDate: 2022
+}
